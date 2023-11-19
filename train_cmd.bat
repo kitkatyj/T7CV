34d17e2e763c4b2c230102c953663e33ce62f375
Tej-Deep <ptejdeep@gmail.com>
2023-11-19 23:54:49 +0800

    Adding FlowNetS Model Architecture

---
+++ train_cmd.bat
@@ -0,0 +1,1 @@
+python main.py --batch_size 32 --test_batch_size 32 --dataset vimeo90K_septuplet --loss 1*L1 --max_epoch 200 --lr 0.0002 --data_root ./vimeo_septuplet --n_outputs 1 --exp_name opt_flow --checkpoint_dir ckpts --num_workers 4
